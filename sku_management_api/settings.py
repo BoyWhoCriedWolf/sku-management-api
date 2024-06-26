b3c7eb3f773e65a68ea4bde3e42169137cfb3a35
BoyWhoCriedWolf <hdreamdev@gmail.com>
2024-06-27 01:00:06 +0800

    fix: rest framework permission

--- sku_management_api/settings.py
+++ sku_management_api/settings.py
@@ -145,11 +145,9 @@ if not DEBUG:
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.DjangoModelPermissions',
+    )
 }
 
 
