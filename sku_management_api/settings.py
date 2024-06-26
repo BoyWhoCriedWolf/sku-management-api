84bf4c994814ce6d2d292738ffb452ed09a297c1
BoyWhoCriedWolf <hdreamdev@gmail.com>
2024-06-27 01:17:01 +0800

    remove: auth configuration

--- sku_management_api/settings.py
+++ sku_management_api/settings.py
@@ -148,9 +148,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.DjangoModelPermissions',
     ),
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
-    ]
+    # 'DEFAULT_AUTHENTICATION_CLASSES': [
+    #     'rest_framework.authentication.BasicAuthentication',
+    # ]
 }
 
 
